b4c3feca026edcfdcb96fc7347eafa7960557dee
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-09-13 12:31:26 +0200

    new updates for seasonal changes

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -38,7 +38,17 @@ const LogIn = () => {
             
 
     }
-
+  // fetch("http://localhost:3000/sessions", {
+    //     method: "POST",
+    //     headers: {
+    //         "content-type": "application/json",
+    //         accepts: "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         password: password,
+    //         email: email,
+    //     })
+    // })
 
 
 
