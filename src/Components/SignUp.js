2598df2d498265b28adc681d37ed6bcbb0de2707
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-09-20 21:59:17 -0400

    start of authentication

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -79,10 +79,6 @@ const SignUp = (props) => {
 
     )
 }
-// fetch("http://localhost:3000/events", {
-    //     method: "POST",
-    //     headers: {
-    //         "content-type": "application/json",
-    //         accepts: "application/json"
+
 
 export default SignUp
